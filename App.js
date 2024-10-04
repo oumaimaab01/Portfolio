fe1f62cec96da3763b0047e46c546cf157cbf0f9
OUMAIMA ABJAOU <oumaima_abjaou@um5.ac.ma>
2024-10-05 00:48:27 +0100

    Files upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Skills from "./Skills";
+const App = () => {
+return (
+    <div className="App">
+      // Nav, Header, and other components should be here. I'm only importing my Skills component for styling. 
+      <Skills />
+    </div>
+  );
+};
+
+export default App;
